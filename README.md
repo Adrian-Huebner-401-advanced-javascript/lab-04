ac67ae22ad1ef3403b5183220c40b73467676a5a
adrianhuebner <adrianh92@gmail.com>
2019-10-17 20:05:38 -0700

    added to README and added more code

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# lab-04
+# LAB - 04
+
+## Data Modeling
+
+### Author: Adrian Huebner
+
+### Links and Resources
+* [submission PR](https://github.com/Adrian-Huebner-401-advanced-javascript/lab-04/pull/1)
+* [travis](https://www.travis-ci.com/Adrian-Huebner-401-advanced-javascript/lab-04)
+* [front-end](https://adrianhuebner-lab-04.herokuapp.com/)
+
+
+### Modules
+#### `categories.js`
+#### `products.js`
+  
+#### Tests
+* How do you run tests?
+`npm run test`
+`npm run lint`
+
+
+#### UML
+![uml for lab-04](/assests/Lab4-UML.png)
--- memory.js
+++ memory.js
@@ -55,3 +55,90 @@ class Model {
 }
 
 module.exports = Model;
+
+// Validator Code that I will figure out
+
+// class Validator{
+//   /**
+//    * Based on a set of rules, is the input valid?
+//    * TODO: Define the rules ... how do we send them in? How do we identify?
+//    * @param input
+//    * @param rules
+//    * @returns {boolean}
+//    */
+//   isValid(input, rules){
+//     return true;
+//   };
+  
+//   /**
+//    * Is this a string?
+//    * @param input
+//    * @returns {boolean}
+//    */
+//   isString (input){
+//     return typeof input === 'string';
+//   };
+  
+//   /**
+//    * Is this a number?
+//    * @param input
+//    * @returns {boolean}
+//   */
+//   isNumber (input){
+//     return typeof input === 'number';
+//   }
+  
+//   /**
+//    * Is this an array?
+//    * @param input
+//    * @returns {boolean}
+//    */
+//   isArray (input){
+//     return Array.isArray(input);
+//   }
+  
+//   /**
+//    * Is this an object?
+//    * @param input
+//    * @returns {boolean}
+//    */
+//   isObject (input) {
+//     return typeof input === 'object';
+//   }
+  
+//   /**
+//    * Is this a boolean?
+//    * @param input
+//    * @returns {boolean}
+//    */
+//   isBool (input) {
+//     return typeof input === 'boolean';
+//   }
+  
+//   /**
+//    * Is this a funcion?
+//    * @param input
+//    * @returns {boolean}
+//    */
+//   isFunc (input){
+//     return typeof input === 'function';
+//   }
+  
+//   /**
+//    * Area all values in array same type?
+//    * @param input
+//    * @returns {boolean}
+//    */
+//   //thanks to Julie for helping me through this
+//   typeArray (input) {
+//     const type = typeof input[0];
+//       for(let i = 1; input.length > 0; i++){
+//         if (typeof input[i] !== type) {
+//         return false;
+//         }
+//         return true;
+//       }
+//   };
+//   }
+  
+//   module.exports = Validator;
